d17850904673f62c0767d1d0c30427ea450baeca
Silthus <michael@reichenbach.in>
2021-03-01 19:48:45 +0100

    fix(plan): show referral type and player in player table

--- src/main/java/de/raidcraft/referrals/plan/ReferralDataExtension.java
+++ src/main/java/de/raidcraft/referrals/plan/ReferralDataExtension.java
@@ -2,6 +2,8 @@ package de.raidcraft.referrals.plan;
 
 import com.djrapitops.plan.extension.CallEvents;
 import com.djrapitops.plan.extension.DataExtension;
+import com.djrapitops.plan.extension.annotation.BooleanProvider;
+import com.djrapitops.plan.extension.annotation.Conditional;
 import com.djrapitops.plan.extension.annotation.PluginInfo;
 import com.djrapitops.plan.extension.annotation.StringProvider;
 import com.djrapitops.plan.extension.icon.Color;
@@ -34,8 +36,10 @@ public class ReferralDataExtension implements DataExtension {
             iconName = "user-friends",
             iconFamily = Family.SOLID,
             iconColor = Color.NONE,
-            playerName = true
+            playerName = true,
+            showInPlayerTable = true
     )
+    @Conditional("isReferredByPlayer")
     public String referredByPlayer(UUID playerUUID) {
 
         Optional<ReferralPlayer> player = ReferralPlayer.byId(playerUUID);
@@ -53,8 +57,10 @@ public class ReferralDataExtension implements DataExtension {
             priority = 4,
             iconName = "handshake",
             iconFamily = Family.SOLID,
-            iconColor = Color.NONE
+            iconColor = Color.NONE,
+            showInPlayerTable = true
     )
+    @Conditional("isReferred")
     public String referredReason(UUID playerUUID) {
 
         Optional<ReferralPlayer> player = ReferralPlayer.byId(playerUUID);
@@ -62,5 +68,27 @@ public class ReferralDataExtension implements DataExtension {
         ReferralPlayer referralPlayer = player.get();
         if (referralPlayer.referral() == null) return null;
         return referralPlayer.referral().type().name();
+    }
+
+    @BooleanProvider(
+            text = "Referred by Player?",
+            conditionName = "isReferredByPlayer"
+    )
+    public boolean isReferredByPlayer(UUID playerUUID) {
+
+        Optional<ReferralPlayer> player = ReferralPlayer.byId(playerUUID);
+        if (player.isEmpty()) return false;
+        return player.get().referral() != null && player.get().referral().isPlayerReferral();
+    }
+
+    @BooleanProvider(
+            text = "Is referred?",
+            conditionName = "isReferred"
+    )
+    public boolean isReferred(UUID playerUUID) {
+
+        Optional<ReferralPlayer> player = ReferralPlayer.byId(playerUUID);
+        if (player.isEmpty()) return false;
+        return player.get().referral() != null;
     }
 }
