48445c6f450a7c5f3444dbd5273a78c9c51e627c
Michael Reichenbach <michael.reichenbach@interhyp.de>
2021-03-18 07:20:26 +0100

    feat: improve plan data extension

--- src/main/java/de/raidcraft/referrals/plan/ReferralDataExtension.java
+++ src/main/java/de/raidcraft/referrals/plan/ReferralDataExtension.java
@@ -2,13 +2,13 @@ package de.raidcraft.referrals.plan;
 
 import com.djrapitops.plan.extension.CallEvents;
 import com.djrapitops.plan.extension.DataExtension;
-import com.djrapitops.plan.extension.annotation.BooleanProvider;
-import com.djrapitops.plan.extension.annotation.Conditional;
-import com.djrapitops.plan.extension.annotation.PluginInfo;
-import com.djrapitops.plan.extension.annotation.StringProvider;
+import com.djrapitops.plan.extension.annotation.*;
 import com.djrapitops.plan.extension.icon.Color;
 import com.djrapitops.plan.extension.icon.Family;
+import com.djrapitops.plan.extension.icon.Icon;
+import com.djrapitops.plan.extension.table.Table;
 import de.raidcraft.referrals.entities.ReferralPlayer;
+import de.raidcraft.referrals.entities.ReferralType;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -25,8 +25,23 @@ public class ReferralDataExtension implements DataExtension {
     public CallEvents[] callExtensionMethodsOn() {
         return new CallEvents[]{
                 CallEvents.PLAYER_JOIN,
-                CallEvents.PLAYER_LEAVE
+                CallEvents.PLAYER_LEAVE,
+                CallEvents.SERVER_EXTENSION_REGISTER
         };
+    }
+
+    @TableProvider()
+    public Table referralStats() {
+
+        Table.Factory factory = Table.builder()
+                .columnOne("Referral Type", Icon.called("handshake").of(Color.BLUE_GREY).of(Family.SOLID).build())
+                .columnTwo("Count", Icon.called("chart-line").of(Color.BLUE_GREY).of(Family.SOLID).build());
+
+        ReferralType.all().stream()
+                .sorted((o1, o2) -> Integer.compare(o2.referrals().size(), o1.referrals().size()))
+                .forEach(referralType -> factory.addRow(referralType.name(), referralType.referrals().size()));
+
+        return factory.build();
     }
 
     @StringProvider(
@@ -51,6 +66,24 @@ public class ReferralDataExtension implements DataExtension {
         return referralPlayer.referral().referredBy().name();
     }
 
+    @NumberProvider(
+            text = "Referral Count",
+            description = "The number of players this player referred.",
+            priority = 4,
+            iconName = "user-friends",
+            iconFamily = Family.SOLID,
+            iconColor = Color.NONE,
+            showInPlayerTable = true
+    )
+    public int referralCount(UUID playerUUID) {
+
+        Optional<ReferralPlayer> player = ReferralPlayer.byId(playerUUID);
+        if (player.isEmpty()) return 0;
+        ReferralPlayer referralPlayer = player.get();
+
+        return referralPlayer.referrals().size();
+    }
+
     @StringProvider(
             text = "Referral Type",
             description = "The reason why the player joined the server.",
@@ -71,7 +104,7 @@ public class ReferralDataExtension implements DataExtension {
     }
 
     @BooleanProvider(
-            text = "Referred by Player?",
+            text = "Referred by Player",
             conditionName = "isReferredByPlayer"
     )
     public boolean isReferredByPlayer(UUID playerUUID) {
@@ -82,7 +115,7 @@ public class ReferralDataExtension implements DataExtension {
     }
 
     @BooleanProvider(
-            text = "Is referred?",
+            text = "Is referred",
             conditionName = "isReferred"
     )
     public boolean isReferred(UUID playerUUID) {
